52f468df2b3b16bd7490c4ea3ce849b642b5dd04
Keane Leong <ac107452@avcol.school.nz>
2020-06-29 10:55:29 +1200

    Added More Pages And Experimented On Layout!

---
+++ KeanesWebApp/Pages/NewsAndEvents.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KeanesWebApp
+{
+    public class NewsAndEventsModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
